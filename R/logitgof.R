a84c2fa4efd335f738bcdbdb464d95c1463d4ce9
matthewjay15 <matthew.jay.15@ucl.ac.uk>
2017-06-24 12:17:28 +0100

    Update logitgof.R

--- R/logitgof.R
+++ R/logitgof.R
@@ -1,6 +1,6 @@
+logitgof <-
 function (obs, exp, g = 10, ord = FALSE) {
-  DNAME <- paste(deparse(substitute(obs)), deparse(substitute(exp)), 
-                 sep = ", ")
+  DNAME <- paste(deparse(substitute(obs)), deparse(substitute(exp)), sep = ", ")
   yhat <- exp
   if (is.null(ncol(yhat))) {
     mult <- FALSE
@@ -21,24 +21,25 @@ function (obs, exp, g = 10, ord = FALSE) {
     dfobs <- data.frame(obs, cutyhats)
     dfobsmelt <- melt(dfobs, id.vars = 2)
     observed <- cast(dfobsmelt, cutyhats ~ value, length)
+    names(observed)[names(observed) != "cutyhats"] <- paste0("x",
+                                                             names(observed)[names(observed) != "cutyhats"])
     observed <- observed[order(c(1, names(observed[, 2:ncol(observed)])))]
     dfexp <- data.frame(yhat, cutyhats)
     dfexpmelt <- melt(dfexp, id.vars = ncol(dfexp))
     expected <- cast(dfexpmelt, cutyhats ~ variable, sum)
     expected <- expected[order(c(1, names(expected[, 2:ncol(expected)])))]
-    stddiffs <- abs(observed[, 2:ncol(observed)] - expected[,2:ncol(expected)])/sqrt(expected[, 2:ncol(expected)])
-    if (ncol(expected) != ncol(observed)) 
-      stop("Observed and expected tables have different number of columns. Check you entered the correct data.")
-    if (any(expected[, 2:ncol(expected)] < 1)) 
+    stddiffs <- abs(observed[, 2:ncol(observed)] - expected[, 2:ncol(expected)]) / sqrt(expected[, 2:ncol(expected)])
+    if (ncol(expected) != ncol(observed)) stop("Observed and expected tables have different number of columns. Check you entered the correct data.")
+    if (any(expected[, 2:ncol(expected)] < 1))
       warning("At least one cell in the expected frequencies table is < 1. Chi-square approximation may be incorrect.")
-    if (any(observed[, 2:ncol(observed)] < 0) || any(expected[, 2:ncol(expected)] < 0)) 
+    if (any(observed[, 2:ncol(observed)] < 0)  || any(expected[, 2:ncol(expected)] < 0))
       stop("Observed or expected values < 0. Check that observed and fitted values entered correctly.")
-    chisq <- sum((observed[, 2:ncol(observed)] - expected[, 2:ncol(expected)])^2/expected[, 2:ncol(expected)])
+    chisq <- sum((observed[, 2:ncol(observed)] - expected[, 2:ncol(expected)])^2 / expected[, 2:ncol(expected)])
     if (!ord) {
-      PARAMETER <- (nrow(expected) - 2) * (ncol(yhat) - 1)
-    } else {
-      PARAMETER <- (nrow(expected) - 2) * (ncol(yhat) - 1) + ncol(yhat) - 2
-    }
+        PARAMETER <- (nrow(expected) - 2) * (ncol(yhat) - 1) 
+      } else {
+        PARAMETER <- (nrow(expected) - 2) * (ncol(yhat) - 1) + ncol(yhat) - 2
+      }
   } else {
     METHOD <- "Hosmer and Lemeshow test (binary model)"
     if (is.factor(obs)) {
@@ -50,14 +51,15 @@ function (obs, exp, g = 10, ord = FALSE) {
     cutyhat <- cut(yhat, breaks = qq, include.lowest = TRUE)
     observed <- xtabs(cbind(y0 = 1 - y, y1 = y) ~ cutyhat)
     expected <- xtabs(cbind(yhat0 = 1 - yhat, yhat1 = yhat) ~ cutyhat)
-    stddiffs <- abs(observed - expected)/sqrt(expected)
-    if (any(expected < 1)) 
+    stddiffs <- abs(observed - expected) / sqrt(expected)
+    if (any(expected < 1))
       warning("At least one cell in the expected frequencies table is < 1. Chi-square approximation may be incorrect.")
-    if (any(observed < 0) || any(expected < 0)) 
+    if (any(observed < 0) || any(expected < 0))
       stop("Observed or expected values < 0. Check that observed and fitted values entered correctly.")
     chisq <- sum((observed - expected)^2/expected)
     PARAMETER <- nrow(expected) - 2
-  } if (g != nrow(expected)) 
+  }
+  if (g != nrow(expected))
     warning(paste("Not possible to compute", g, "rows. There might be too few observations."))
   PVAL <- 1 - pchisq(chisq, PARAMETER)
   names(chisq) <- "X-squared"
